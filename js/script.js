684d61a210a73c58cbea46bb99e3c93452185c76
Thaynara Guimarães <thaynaraelinezdsf@gmail.com>
2023-02-15 19:25:18 -0300

    feat: :construction: navebar mobile, responsividade mobile
    inicio da construção do navebar mobile, e contrução da responsividade da tela inicial

--- js/script.js
+++ js/script.js
@@ -1,1 +1,8 @@
-$
+$(document).ready(function(){
+    $(window).scroll(function(){
+        if(this.scrollY > 20){
+            $('.navbarmenu').addClass("sticky");
+        }else{
+            $('.navbarmenu').removeClass("sticky");}
+    })
+})
